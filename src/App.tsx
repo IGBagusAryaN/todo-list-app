d28db6b8d34c791f1e6c57ac0bc29570145497cf
Bagusarya <aryabagus453@gmail.com>
2025-09-27 19:03:04 +0700

    feat: update App.tsx to render tasks page as main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import TaskForm from "./pages/task-add";
-import TaskList from "./pages/task-list";
+import TaskPage from "./pages/task-page";
 
 function App() {
   return (
     <>
       <div className="min-h-screen p-2 md:p-6 max-w-7xl mx-auto pt-10 lg:pt-20 ">
-        <h1 className="text-2xl font-bold mb-4 px-3 md:px-0">To-Do</h1>
-        <div className="block lg:grid grid-cols-[1fr_2fr]">
-        <TaskForm />
-        <TaskList />
-        </div>
+        <TaskPage/>
       </div>
     </>
   );
